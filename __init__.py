754b10857d9ffbabec90700afb2271651c637cea
Marcel Wendler <marcel.wendler008@gmail.com>
2026-07-30 22:04:41 +0200

    Set path to vendor/

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
-from .setup import setup_addon
+from pathlib import Path
+import sys
 
+VENDOR_DIR = Path(__file__).resolve().parent / "vendor"
+if str(VENDOR_DIR) not in sys.path:
+    sys.path.insert(0, str(VENDOR_DIR))
+
+from .setup import setup_addon
 
 setup_addon()
