d211334a814c0d120eb681bdface67afd9550bb5
Marcel Wendler <marcel.wendler008@gmail.com>
2026-07-30 17:45:05 +0200

    Remove fetch_kanji_meanings

--- domain/kanji.py
+++ domain/kanji.py
@@ -1,19 +1,6 @@
 import unicodedata
 
 
-def fetch_kanji_meanings(kanji: str) -> list[str]:
-    """Fetch English meanings for a kanji from Jamdict's Chars section."""
-    if not kanji:
-        return []
-
-    meanings = globals.kanji_dictionary.get(kanji)
-
-    if not meanings:
-        return []
-
-    return meanings
-
-
 def is_kanji(char):
     """Return True if char is a kanji."""
     try:
