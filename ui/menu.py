317f168872e5c4fa910701c211f0cdb1c8378354
Marcel Wendler <marcel.wendler008@gmail.com>
2026-08-01 11:51:18 +0200

    Move setup_menu into seperate file

---
+++ ui/menu.py
@@ -0,0 +1,45 @@
+from aqt import mw
+from aqt.qt import QAction, QMenu
+
+
+def setup_menu(config, collection_service, show_todays_words, show_settings):
+    """Set up the JapaneseMining menu in Anki's Tools menu."""
+    my_menu = QMenu("JapaneseMining", mw)
+
+    action = QAction("Show Today's Words", mw)
+    action.triggered.connect(show_todays_words)
+    my_menu.addAction(action)
+
+    action = QAction("Settings", mw)
+    action.triggered.connect(show_settings)
+    my_menu.addAction(action)
+
+    my_menu.addSeparator()
+
+    action = QAction("Soft Update Everything", mw)
+    action.triggered.connect(collection_service.update_japanese_mining_cards)
+    my_menu.addAction(action)
+
+    action = QAction("Force Update Keywords", mw)
+    action.triggered.connect(collection_service.force_update_keywords)
+    my_menu.addAction(action)
+
+    action = QAction("Force Update Meanings", mw)
+    action.triggered.connect(collection_service.force_update_meanings)
+    my_menu.addAction(action)
+
+    action = QAction("Force Update Everything", mw)
+    action.triggered.connect(collection_service.force_update_everything)
+    my_menu.addAction(action)
+
+    my_menu.addSeparator()
+    action = QAction("Add Unknown Kanji", mw)
+    action.triggered.connect(collection_service.add_unknown_kanji)
+    my_menu.addAction(action)
+
+    my_menu.addSeparator()
+    action = QAction("Export Learned Kanji", mw)
+    action.triggered.connect(collection_service.export_learned_kanji)
+    my_menu.addAction(action)
+
+    mw.form.menuTools.addMenu(my_menu)
